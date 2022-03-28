d1ff3aec54d75ece7a79023dee0980b6a40180fa
D20CM <scottbrown500@hotmail.com>
2022-03-28 13:10:55 +0100

    finish crud api routes

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-import getAllUsers from '../models/users';
+import {getAllUsers, getUserById, addUser, updateUser, deleteUser} from '../models/users';
 
 const usersRouter = express.Router();
 usersRouter.use(express.json());
@@ -10,6 +10,31 @@ usersRouter.use(express.json());
 usersRouter.get('/', function(req, res) {
   const allUsers = await getAllUsers();
   res.json(allUsers);
+});
+
+usersRouter.get("/:id", async function (req, res) {
+  let id = req.params.id;
+  const user = await getUserById(id);
+  res.json(user);
+});
+
+usersRouter.post("/", async function (req, res) {
+  let newUser = req.body;
+  const addedUser = await addUser(newUser);
+  res.json(addedUser);
+});
+
+usersRouter.put("/:id", async function (req, res) {
+  let id = req.params.id;
+  let newData = req.body;
+  const updatedUser = await updateUser(id, newData);
+  res.json(updatedUser);
+});
+
+usersRouter.delete("/:id", async function (req, res) {
+  let id = req.params.id;
+  const deletedUser = await deleteUser(id);
+  res.json(deletedUser);
 });
 
 module.exports = usersRouter;
