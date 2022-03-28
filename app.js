fcfcdeed0d4a24d649d4c2b06d27983469476dc2
D20CM <scottbrown500@hotmail.com>
2022-03-28 14:07:29 +0100

    views in app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ import usersRouter from "./routes/users.js";
 var app = express();
 
 // view engine setup
-// app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
