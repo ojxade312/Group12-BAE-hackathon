da9e8a92aa9660744ec28c61faabe4929103c96f
D20CM <scottbrown500@hotmail.com>
2022-03-28 14:19:06 +0100

    change error handling

--- app.js
+++ app.js
@@ -28,18 +28,15 @@ app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+  res.status(404).json({
+    message: "We couldn't find what you were looking for 😞",
+  });
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  console.error(err.stack);
+  res.status(500).json(err);
 });
 
 export default app;
