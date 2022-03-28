b6b4b0346c637ffff7940521b71833169891b6c5
D20CM <scottbrown500@hotmail.com>
2022-03-28 14:21:16 +0100

    revert

--- app.js
+++ app.js
@@ -28,15 +28,18 @@ app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  res.status(404).json({
-    message: "We couldn't find what you were looking for 😞",
-  });
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).json(err);
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 export default app;
